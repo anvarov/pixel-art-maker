b5262aa268512465d87a57956dedf4defa8d2680
anvarov <akmalanvaroff@gmail.com>
2018-01-28 03:10:27 -0600

    Some code refactoring, and changed identifier declaration to const

--- designs.js
+++ designs.js
@@ -2,11 +2,10 @@ const formInput = document.getElementById('sizePicker')
 // adding event listener for form submit
 formInput.addEventListener('submit', makeGrid, false);
 // event delegation with target on 'td' element
-document.querySelector('body').addEventListener('mousedown', changeColor);
 
 function changeColor(e) {
-    let target = e.target;
-    let color = document.getElementById('colorPicker').value;
+    const target = e.target;
+    const color = document.getElementById('colorPicker').value;
     // if a cell has not been painted, set a new color
     if (target.tagName.toLowerCase() == 'td' && target.getAttribute('class') != 'painted') {
         target.bgColor  = color;
@@ -20,17 +19,18 @@ function changeColor(e) {
 }
 
 function makeGrid(event) {
+    event.preventDefault();
     // removing previous grid
     document.getElementById('pixel_canvas').remove();
     // creating a new grid
-    let table = document.createElement('table');
+    const table = document.createElement('table');
     // traversing to the last h2 element 'Design Canvas'
-    let last_h2 = document.getElementsByTagName('h2')[2];
+    const last_h2 = document.getElementsByTagName('h2')[2];
     table.setAttribute('id', 'pixel_canvas');
     // inserting after last h2 element new grid as a sibling
     last_h2.after(table);
-    let height = formInput.height.value;
-    let width = formInput.width.value;
+    const height = formInput.height.value;
+    const width = formInput.width.value;
     for (let i = 0; i < height; i++) {
         let tr = document.createElement('tr');
         table.appendChild(tr);
@@ -39,5 +39,5 @@ function makeGrid(event) {
             tr.appendChild(td);
         }
     }
-    event.preventDefault();
+    document.querySelector('table').addEventListener('mousedown', changeColor);
 }
